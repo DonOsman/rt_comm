2200d64bd0c4582be58120e8843ac31bdd907554
DonOsman <37080469+DonOsman@users.noreply.github.com>
2020-01-22 19:10:16 -0500

    add job and comment models
    Adding potential models for both the Job and Comments

---
+++ RTComm/Data/Jobs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTComm.Data
+{
+    public class Jobs
+    {
+        public int JobId { get; set; }
+        public string JobName { get; set; }
+        public string JobCategory { get; set; }
+        public decimal Cost { get; set; }
+        public string ConstructionCompany { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ClientName { get; set; }
+        public ICollection <Comments> Comments { get; set; }
+
+    }
+}
